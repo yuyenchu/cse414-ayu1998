7995972aaf3fcb9cc73029e055f3c40815a59188
andrew <andrew7011616@gmail.com>
2019-05-03 00:02:01 -0700

    update 0503
    Former-commit-id: 3a21583e1c451a78f04181101fb77ac1c699f635

--- hw/hw2/submission/hw2-q4.sql
+++ hw/hw2/submission/hw2-q4.sql
@@ -4,6 +4,6 @@ select distinct c.name
     from FLIGHTS as f
     inner join CARRIERS as c
         on f.carrier_id = c.cid
-    group by f.month_id, f.day_of_month, f.carrier_id
-    having count(f.carrier_id) > 1000
+    group by f.carrier_id, f.month_id, f.day_of_month
+    having count(f.fid) > 1000
     order by name;
