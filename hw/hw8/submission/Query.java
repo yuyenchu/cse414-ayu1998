a1b7d44b717dbb42cc1d68e22788707dc341f5a2
andrew <andrew7011616@gmail.com>
2019-06-05 19:55:02 -0700

    update 0605-3
    Former-commit-id: d002106d1c8f227b1e1fe1e3ffdb192b6f399819

--- hw/hw8/submission/Query.java
+++ hw/hw8/submission/Query.java
@@ -5,7 +5,7 @@ public class Query extends QuerySearchOnly {
 	//debug mode
 	private static final boolean DEBUG = false;
 
-	// Logged In User
+	// Logged In User's username
 	private String username; // customer username is unique
 
 	// transactions
@@ -146,7 +146,6 @@ public class Query extends QuerySearchOnly {
 		updateReservationCancelStatement = conn.prepareStatement(UPDATE_RESERVATION_CANCEL_SQL);
 		updateReservationPaidStatement = conn.prepareStatement(UPDATE_RESERVATION_PAID_SQL);
 		//get statements
-		//get statements
 		getReservationsStatement = conn.prepareStatement(GET_RESERVATIONS_SQL);
 		getFlightStatement = conn.prepareStatement(GET_FLIGHT_SQL);
 		getBalanceStatement = conn.prepareStatement(GET_BALANCE_SQL);
@@ -156,7 +155,6 @@ public class Query extends QuerySearchOnly {
 		//clear statements
 		deleteTravelStatement = conn.prepareStatement(DELETE_TRAVEL_SQL);
 		clearStatement = conn.prepareStatement(CLEAR_SQL);
-
 	}
 
 
@@ -177,7 +175,9 @@ public class Query extends QuerySearchOnly {
 			return "Login failed\n" + (DEBUG ? "1\n" : "");
 		}
 		try {
+			//check username ans password match
 			if (checkLogin(username, password)) {
+				//set usernname and clean search itinerary
 				this.username = username;
 				itineraries = new ArrayList<>();
 				return "Logged in as " + username + "\n";
@@ -203,12 +203,15 @@ public class Query extends QuerySearchOnly {
 	 */
 	public String transaction_createCustomer (String username, String password, int initAmount)
 	{
+		//checking parameters
 		if (username == null || password == null || username.isEmpty() || password.isEmpty() || initAmount < 0) {
 			return "Failed to create user\n" + (DEBUG ? "1\n" : "");
 		}
 		try {
 			beginTransaction();
+			//check username isn't used
 			if (!checkUserame(username)) {
+				//create new user
 				insertUser(username, password, initAmount);
 				commitTransaction();
 				return "Created user " + username + "\n";
@@ -249,9 +252,11 @@ public class Query extends QuerySearchOnly {
 		} 
 		try {
 			beginTransaction();
+			//get data of flight(s) from search itinerary
 			Fid2 temp = itineraries.get(itineraryId);
 			Flight f1 = getFlight(temp._1);
 			Flight f2 = temp._2 > 0 ? getFlight(temp._2) : null;
+			//checking date and capacity
 			if (checkTravel(f1.month, f1.dayOfMonth) || (f2 != null && checkTravel(f2.month, f2.dayOfMonth))) {
 				rollbackTransaction();
 				return "You cannot book two flights in the same day\n";
@@ -259,6 +264,7 @@ public class Query extends QuerySearchOnly {
 				rollbackTransaction();
 				return "Booking failed\n" + (DEBUG ? "1\n" : "");
 			}
+			//creating new Reservation 
 			int id = insertReservation(f1, f2);
 			insertTravel(f1.month, f1.dayOfMonth);
 			if (f2 != null) {
@@ -297,21 +303,25 @@ public class Query extends QuerySearchOnly {
 		} 
 		try {
 			beginTransaction();
+			//checking Reservation is valid
 			if (!checkUnpaidReservation(reservationId) || !checkNotCanceledReservation(reservationId)) {
 				rollbackTransaction();
 				return "Cannot find unpaid reservation " + reservationId + " under user: " + this.username + "\n";
 			} 
+			//get price of Reservation
 			getReservationStatement.clearParameters();
 			getReservationStatement.setString(1, this.username);
 			getReservationStatement.setInt(2, reservationId);
 			ResultSet result = getReservationStatement.executeQuery();
 			result.next();
+			//make sure user's balance is enough
 			int balance = getBalance();
 			if (balance < result.getInt(7)) {
 				rollbackTransaction();
 				return "User has only " + balance + " in account but itinerary costs " + result.getInt(7) + "\n";
 			}
 			balance -= result.getInt(7);
+			//update subtracted balance, and set Reservation.paid = 1
 			updateBalance(balance);
 			payReservation(result.getInt(1));
 			result.close();
@@ -354,6 +364,7 @@ public class Query extends QuerySearchOnly {
 		}
 		String out = "";
 		try {
+			//get all uncanceled Reservations
 			getReservationsStatement.clearParameters();
 			getReservationsStatement.setString(1, this.username);
 			ResultSet result = getReservationsStatement.executeQuery();
@@ -364,6 +375,7 @@ public class Query extends QuerySearchOnly {
 					out += getFlight(result.getInt(4)).toString() + "\n";
 				}
 			}
+			//checking is result empty
 			return out.equals("") ? "No reservations found\n" : out;
 		} catch (Exception e) {
 			if (DEBUG) {
@@ -388,14 +400,22 @@ public class Query extends QuerySearchOnly {
 	 */
 	public String transaction_cancel(int reservationId)
 	{
-		// only implement this if you are interested in earning extra credit for the HW!
 		if (this.username == null) {
 			return "Cannot cancel reservations, not logged in\n";
 		}
 		try {
 			beginTransaction();
 			if (checkNotCanceledReservation(reservationId)){
+				//canceling Reservation, set cancel = 1
 				cancelReservation(reservationId);
+				//refund for canceled Reservation
+				getReservationStatement.clearParameters();
+				getReservationStatement.setString(1, this.username);
+				getReservationStatement.setInt(2, reservationId);
+				ResultSet result = getReservationStatement.executeQuery();
+				result.next();
+				updateBalance(getBalance() + result.getInt(7));
+				result.close();
 				commitTransaction();
 				return "Canceled reservation " + reservationId + "\n";
 			} 
@@ -436,6 +456,11 @@ public class Query extends QuerySearchOnly {
 		}
 	}
 
+	/**
+	 * checking if given name exists in table Users, searching case-insensitively
+	 * @param username to check
+	 * @return true if name exist in table Users, false if not
+	 */
 	private boolean checkUserame(String name) throws SQLException {
 		checkUserameStatement.clearParameters();
 		checkUserameStatement.setString(1, name);
@@ -449,6 +474,11 @@ public class Query extends QuerySearchOnly {
 		return out;
 	}
 	
+	/**
+	 * checking if given name and password matches, searching case-insensitively
+	 * @param username and password to check
+	 * @return true if name and password tuple exist in table Users, false if not
+	 */
 	private boolean checkLogin(String name, String password) throws SQLException {
 		checkLoginStatement.clearParameters();
 		checkLoginStatement.setString(1, name);
@@ -463,6 +493,12 @@ public class Query extends QuerySearchOnly {
 		return out;
 	}
 
+	/**
+	 * checking if reservation of given id is unpaid
+	 * @param id of reservation to check
+	 * @return true if there is 1 reservation of given id and current user with 
+	 * 		   paid attribute equals 0 exists in table Reservations, false if not
+	 */
 	private boolean checkUnpaidReservation(int id) throws SQLException {
 		checkUnpaidReservationStatement.clearParameters();
 		checkUnpaidReservationStatement.setInt(1, id);
@@ -477,6 +513,12 @@ public class Query extends QuerySearchOnly {
 		return out;
 	}
 
+	/**
+	 * checking if given date and month tuple of current user exists
+	 * @param month and date to check, assuming both valid and user is logged in
+	 * @return true if there is 1 tuple match month, day, and current user's username
+	 * 		   in Travel, false if not
+	 */
 	private boolean checkTravel(int month, int day) throws SQLException {
 		checkTravelStatement.clearParameters();
 		checkTravelStatement.setInt(1, month);
@@ -492,6 +534,12 @@ public class Query extends QuerySearchOnly {
 		return out;
 	}
 
+	/**
+	 * checking if reservation of given id is not canceled
+	 * @param id of reservation to check
+	 * @return true if tere is 1 reservation of given id and current user with 
+	 * 		   canceled attribute equals 0 exists in table Reservations, false if not
+	 */
 	private boolean checkNotCanceledReservation(int id) throws SQLException {
 		checkNotCanceledReservationStatement.clearParameters();
 		checkNotCanceledReservationStatement.setInt(1, id);
@@ -506,6 +554,10 @@ public class Query extends QuerySearchOnly {
 		return out;
 	}
 
+	/**
+	 * insert a new tuple into Users
+	 * @param username, password, and initial balance for new tuple, assuming all valid
+	 */
 	private void insertUser(String name, String password, int balance) throws SQLException {
 		insertUserStatement.clearParameters();
 		insertUserStatement.setString(1, name);
@@ -514,6 +566,10 @@ public class Query extends QuerySearchOnly {
 		insertUserStatement.executeUpdate();
 	}
 
+	/**
+	 * insert a new tuple into Travel of current user, assuming user is logged in
+	 * @param month_id and day_of_month for new tuple, assuming all valid
+	 */
 	private void insertTravel(int month, int day) throws SQLException {
 		insertTravelStatement.clearParameters();
 		insertTravelStatement.setInt(1, month);
@@ -522,10 +578,18 @@ public class Query extends QuerySearchOnly {
 		insertTravelStatement.executeUpdate();
 	}
 
+	/**
+	 * insert a new tuple into Reservations of current user and given fligt data
+	 * @param flight data for new reservation, assuming both valid, f2 can be null
+	 * 		  if te reservation only have 1 flight
+	 * @return rid of new reservation tuple
+	 */
 	private int insertReservation(Flight f1, Flight f2) throws SQLException {
 		insertReservationStatement.clearParameters();
+		//get last rid and update it wit increment of 1
 		int id = getRID();
 		updateRID(id + 1);
+		//insert new tuple, set attribute flight2 to NULL if only contain 1 flight
 		insertReservationStatement.setInt(1, id+1);
 		insertReservationStatement.setString(2, this.username);
 		insertReservationStatement.setInt(3, f1.fid);
@@ -541,6 +605,10 @@ public class Query extends QuerySearchOnly {
 		return id + 1;
 	}
 
+	/**
+	 * get te rid of last reservation, assuming at most 1 tuple in table RID  
+	 * @return id of tuple RID, if no tuple exists, return 0
+	 */
 	private int getRID() throws SQLException {
 		ResultSet result = getRidStatement.executeQuery();
 		if (result.next()) {
@@ -558,6 +626,10 @@ public class Query extends QuerySearchOnly {
 		}
 	}
 
+	/**
+	 * get balance of current user, assuming user is logged in
+	 * @return balance of current user, searching case-insensitively
+	 */
 	private int getBalance() throws SQLException {
 		getBalanceStatement.clearParameters();
 		getBalanceStatement.setString(1, this.username);
@@ -568,6 +640,10 @@ public class Query extends QuerySearchOnly {
 		return out;
 	}
 
+	/**
+	 * get flight data of given fid, assuming fid is valid
+	 * @return flight data in object Flight
+	 */
 	private Flight getFlight(int fid) throws SQLException{
 		getFlightStatement.clearParameters();
 		getFlightStatement.setInt(1, fid);
@@ -578,6 +654,12 @@ public class Query extends QuerySearchOnly {
 		return out;
 	}
 
+	/**
+	 * get number of reservations include a flight of given fid
+	 * @param fid of fligt to search, assuming valid
+	 * @return number of tuples in table Reservations has the given
+	 * 		   fid in either attribute flight1 or flight2
+	 */
 	private int getNumReservations(int fid) throws SQLException {
 		getNumReservationsStatement.clearParameters();
 		getNumReservationsStatement.setInt(1, fid);
@@ -589,6 +671,10 @@ public class Query extends QuerySearchOnly {
 		return out;
 	}
 
+	/**
+	 * update the balance of current user, assuming user is logged in
+	 * @param balance to update, assuming non-negative
+	 */
 	private void updateBalance(int balance) throws SQLException {
 		updateBalanceStatement.clearParameters();
 		updateBalanceStatement.setInt(1, balance);
@@ -596,6 +682,11 @@ public class Query extends QuerySearchOnly {
 		updateBalanceStatement.executeUpdate();
 	}
 
+	/**
+	 * update the id of tuple, if no tuple in table RID, ten insert a 
+	 * tuple with id equals to 1, assuming at most 1 tuple in table RID
+	 * @param id to update, assuming an increment of 1 of current id
+	 */
 	private void updateRID(int id) throws SQLException {
 		if (id <= 1) {
 			insertRidStatement.executeUpdate();
@@ -610,6 +701,12 @@ public class Query extends QuerySearchOnly {
 		}
 	}
 
+	/**
+	 * set canceled attribut of the tuple match given id in table
+	 * Reservations to 1, and delete the tuple in table Travel on
+	 * the date of flight(s) contained in the canceled tuple
+	 * @param rid of the tuple to update, assuming valid
+	 */
 	private void cancelReservation(int id) throws SQLException {
 		updateReservationCancelStatement.clearParameters();
 		updateReservationCancelStatement.setInt(1, id);
@@ -627,12 +724,22 @@ public class Query extends QuerySearchOnly {
 		}
 	}
 
+	/**
+	 * set paid attribut of the tuple match given id in table
+	 * Reservations to 1
+	 * @param rid of the tuple to update, assuming valid
+	 */
 	private void payReservation(int id) throws SQLException {
 		updateReservationPaidStatement.clearParameters();
 		updateReservationPaidStatement.setInt(1, id);
 		updateReservationPaidStatement.executeUpdate();
 	}
 
+	/**
+	 * delete the tuple match mont and day of current user in table 
+	 * Travel, assuming user is logged in
+	 * @param month_id and day_of_month of the tuple to delete, assuming valid
+	 */
 	private void deleteTravel(int month, int day) throws SQLException {
 		deleteTravelStatement.clearParameters();
 		deleteTravelStatement.setInt(1, month);
